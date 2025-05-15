29b2cc6c8d2cf24b7792d1781ca5ead6c2dc5aef
Steve Jones <steve@stevejonesdev.com>
2025-05-15 15:48:06 -0400

    added: admin settings class and initialize settings for the plugin

--- includes/AccessibilityPauseAnimatedGifs.php
+++ includes/AccessibilityPauseAnimatedGifs.php
@@ -46,6 +46,12 @@ class AccessibilityPauseAnimatedGifs {
 	public function boot() {
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_styles' ] );
+
+		// Initialize admin settings.
+		if ( is_admin() ) {
+			$settings = new Admin\Settings();
+			$settings->init();
+		}
 	}
 
 	/**
@@ -61,41 +67,44 @@ class AccessibilityPauseAnimatedGifs {
 			EDAPAD_VERSION,
 			true
 		);
+
+		$options         = get_option( 'edapag_options', [] );
+		$default_options = [
+			'buttonBackground'      => '#072c7c',
+			'buttonBackgroundHover' => '#0a2051',
+			'buttonBorder'          => '2px solid #fff',
+			'buttonBorderRadius'    => '0%',
+			'buttonIconColor'       => 'white',
+			'buttonFocusColor'      => '#00e7ffad',
+			'buttonIconSize'        => '1.5rem',
+			'buttonIconFontSize'    => '1rem',
+			'buttonPlayIconID'      => '',
+			'buttonPauseIconID'     => '',
+			'buttonPlayIconHTML'    => '',
+			'buttonPauseIconHTML'   => '',
+			'container'             => 'body',
+			'exclusions'            => '',
+			'gifa11yOff'            => '',
+			'inheritClasses'        => true,
+			'initiallyPaused'       => false,
+			'langPause'             => __( 'Pause animation:', 'accessibility-pause-animated-gifs' ),
+			'langPlay'              => __( 'Play animation:', 'accessibility-pause-animated-gifs' ),
+			'langPauseAllButton'    => __( 'Pause all animations', 'accessibility-pause-animated-gifs' ),
+			'langPlayAllButton'     => __( 'Play all animations', 'accessibility-pause-animated-gifs' ),
+			'langAltWarning'        => __( 'Error! Please add alt text to gif.', 'accessibility-pause-animated-gifs' ),
+			'missingAltWarning'     => false,
+			'sharedPauseButton'     => false,
+			'showButtons'           => true,
+			'showGifText'           => false,
+			'target'                => '',
+			'useDevicePixelRatio'   => true,
+		];
+
 		wp_localize_script(
 			'accessibility-pause-animated-gifs',
 			'edapag',
 			[
-				'options' => [
-					'buttonBackground'      => '#072c7c',
-					'buttonBackgroundHover' => '#0a2051',
-					'buttonBorder'          => '2px solid #fff',
-					'buttonBorderRadius'    => '50%',
-					'buttonIconColor'       => 'white',
-					'buttonFocusColor'      => '#00e7ffad',
-					'buttonIconSize'        => '1.5rem',
-					'buttonIconFontSize'    => '1rem',
-					'buttonPlayIconID'      => '',
-					'buttonPauseIconID'     => '',
-					'buttonPlayIconHTML'    => '',
-					'buttonPauseIconHTML'   => '',
-					'container'             => 'body',
-					'exclusions'            => '',
-					'gifa11yOff'            => '',
-					'inheritClasses'        => true,
-					'initiallyPaused'       => false,
-					'langPause'             => __( 'Pause animation:', 'accessibility-pause-animated-gifs' ),
-					'langPlay'              => __( 'Play animation:', 'accessibility-pause-animated-gifs' ),
-					'langPauseAllButton'    => __( 'Pause all animations', 'accessibility-pause-animated-gifs' ),
-					'langPlayAllButton'     => __( 'Play all animations', 'accessibility-pause-animated-gifs' ),
-					'langMissingAlt'        => __( 'Missing image description.', 'accessibility-pause-animated-gifs' ),
-					'langAltWarning'        => __( 'Error! Please add alt text to gif.', 'accessibility-pause-animated-gifs' ),
-					'missingAltWarning'     => true,
-					'sharedPauseButton'     => false,
-					'showButtons'           => true,
-					'showGifText'           => false,
-					'target'                => '',
-					'useDevicePixelRatio'   => false,
-				],
+				'options' => wp_parse_args( $options, $default_options ),
 			]
 		);
 	}
